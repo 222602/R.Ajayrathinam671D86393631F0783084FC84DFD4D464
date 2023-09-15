76323a3dc8e4e2c7302610e8af42536053bd4dcd
222602 <144416000+222602@users.noreply.github.com>
2023-09-15 20:03:42 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,13 @@
-def isleapyear(year):
-  if (year% 4==0 and year%100!=0) or year %400==0:
-    return True
-  else:
-    return False
-year = int(input("Enter a year:"))  
-if isleapyear(year):
-  print('{}is a loop year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+class player :
+  def play (self):
+    print("The player is playing cricket.")
+class Batsman(player):
+  def play (self):
+   print("The Batsman is batting.")
+class Bowler (player):
+  def play (self):
+    print("The Bowler is bowling.")
+batsman= Batsman()
+bowler= Bowler()
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
-number=int(input("Enter a value")) 
-res=fact_rec(number)
-print("the factorial of {} is {}.".format(number,res))
+class bankaccount:
+  def __init__(self,__accountno, __accountname, initiate_balance=0.0):
+   self.__accountno =__accountno     
+   self.__accountname= __accountname
+   self.__accountbalance=initiate_balance
+  def deposit (self,amount):
+    if amount>0:
+      self.__accountbalance+=amount
+      print("deposit amount₹{}. account balance ₹{}".format(amount,self.__accountbalance))
+    else:
+      print("invalid deposit amount {}".format(amount))
+  def withdraw (self,amount):
+      if amount > 0 and amount>=self.__accountbalance :
+        self.__accountbalance-=amount
+        print("withdraw amount ,₹{}.New balance ₹{}".format(amount,self.__accountbalance))
+      else:
+       print("invalid amount")
+  def display_balance(self):
+    print("Account balance .Account Name{}.Account Number{}. Account balance{}".format(self.__accountname,self.__accountno,self.__accountbalance))
+account =bankaccount (1245780,"logesh",5000.0)
+account.display_balance()
+account.deposit(2000)
+account.withdraw(1500)
+account.display_balance()
